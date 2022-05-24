c222cc39df32387bd3fa7436e2b95261cc7c5dcc
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-05-24 20:05:35 +0300

    PostScreen fixes

--- app/screens/PostScreen.js
+++ app/screens/PostScreen.js
@@ -27,11 +27,21 @@ export default function PostScreen({ postId, navigation }) {
         }
     };
 
+    const getRefreshedPostData = async () => {
+        if (hasAccount) {
+            return await snoo.getSubmission(postId).refresh();
+        } else {
+            const userless = await doUserlessAction();
+            return await userless.getSubmission(postId).refresh();
+        }
+    };
+
     const onRefresh = () => {
         setRefreshing(true);
-        getPostData()
+        getRefreshedPostData()
             .then(postData => {
                 setPostData(postData);
+                setComments(postData.comments);
             })
             .finally(() => {
                 setRefreshing(false);
@@ -46,25 +56,23 @@ export default function PostScreen({ postId, navigation }) {
 
     const Post = () => {
         return (
-            <View>
-                <View style={styles.postContainer}>
-                    <PostComponent postData={{ ...postData, navigation }} isPostScreen={true} />
-                </View>
-                <View>
-                    {comments.length > 0 &&
-                        comments.map(comment => (
-                            <CommentComponent
-                                key={comment.id}
-                                commentData={comment}
-                                navigation={navigation}
-                            />
-                        ))}
-                </View>
+            <View style={styles.postContainer}>
+                <PostComponent postData={{ ...postData, navigation }} isPostScreen={true} />
             </View>
         );
     };
 
-    return <FlatList refreshing={refreshing} onRefresh={onRefresh} ListHeaderComponent={Post} />;
+    return (
+        <FlatList
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            ListHeaderComponent={Post}
+            data={comments}
+            renderItem={({ item }) => (
+                <CommentComponent key={item.id} commentData={item} navigation={navigation} />
+            )}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
